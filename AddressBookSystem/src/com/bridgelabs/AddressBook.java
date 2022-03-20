5c33f8ded50380bafc68fc6dbf9050c82c4faad8
csumn <csumn13@gmail.com>
2022-03-20 15:06:28 +0530

    Final UC added

--- AddressBookSystem/src/com/bridgelabs/AddressBook.java
+++ AddressBookSystem/src/com/bridgelabs/AddressBook.java
@@ -1,12 +1,13 @@
 package com.bridgelabs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBook {
 	Scanner sc = new Scanner(System.in);
 	ArrayList<Contacts> contactlist = new ArrayList<>();
-
+	private HashMap<String, ArrayList<Contacts>> addressBook = new HashMap<>();
 	public void addNewContact()
 	{
 		Contacts contact = new Contacts();
@@ -36,6 +37,19 @@ public class AddressBook {
 
 		contactlist.add(contact);
 		System.out.println("Contact Added Successfully");
+
+		System.out.println("Enter a book name to which you want to add the contact..");
+		String bookName = sc.next();
+		if(addressBook.containsKey(bookName)) {
+			ArrayList<Contacts> contactList = addressBook.get(bookName);
+			addressBook.put(bookName, contactList);
+			System.out.println("New Contact added to the " +bookName);
+		} else {
+			ArrayList<Contacts> contactList = addressBook.get(bookName);
+			addressBook.put(bookName, contactList);
+			System.out.println(bookName+ " created ");
+			System.out.println("New Contact added to the " +bookName);
+		}	
 	}
 
 	public void editContact()
@@ -103,9 +117,10 @@ public class AddressBook {
 					Contacts contact = contactlist.get(i);
 					contactlist.remove(contact);
 					System.out.println("Contact deleted Successfully...");
+					break;
 				}
 				else
-					System.out.println("No such contact exist here....");
+					System.out.println("Contact not found.... ");
 			}
 		}else System.out.println("There are no contacts as of now ......");
 
